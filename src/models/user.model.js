2b8304d12071d7fc05ee74b26c6959ab4edfbfc1
devxutkarsh <dixitu57@gmail.com>
2026-03-20 23:08:51 +0530

    bcrypt and jwt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from 'bcrypt'
 
 const userSchema = new Schema(
   {
@@ -48,5 +50,15 @@ const userSchema = new Schema(
     timestamps: true // ✅ correct place
   }
 );
+
+import bcrypt from "bcrypt";
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
 
 export const User = mongoose.model("User", userSchema);
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,6 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
+
 const videoSchema = new Schema(
   {
     title: {
@@ -42,5 +43,6 @@ const videoSchema = new Schema(
     timestamps: true
   }
 );
+
 
 export const Video = mongoose.model("Video", videoSchema);
